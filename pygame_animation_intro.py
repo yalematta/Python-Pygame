d59874ea1c4f6efbb66c6e866593bdd9a27e1905
yalisme <layale.matta@live.co.uk>
2015-06-27 20:02:35 +0300

    added introduction to animation

---
+++ pygame_animation_intro.py
@@ -0,0 +1,78 @@
+"""
+
+ Show how to use a sprite backed by a graphic. 
+ Sample Python/Pygame Programs
+
+"""
+ 
+import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Starting x position of the rectangle
+# Note how this is outside the main while loop.
+rect_x = 50
+rect_y = 50
+
+# Speed and direction of rectangle
+rect_change_x = 5
+rect_change_y = 5
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(BLACK)
+
+    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
+    
+    # Draw a red rectangle inside the white one
+    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
+    pygame.draw.rect(screen, RED, [rect_x + 10, rect_y + 10 ,30, 30])
+    
+    rect_x += rect_change_x
+    rect_y += rect_change_y
+
+    # Bounce the rectangle if needed
+    if rect_y > 450 or rect_y < 0:
+        rect_change_y = rect_change_y * -1
+    if rect_x > 650 or rect_x < 0:
+        rect_change_x = rect_change_x * -1
+    
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit()
